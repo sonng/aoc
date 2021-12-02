b8fade861b657eded9d84d2c5740d55310a04cf0
Son Nguyen <anh.s.nguyen@gmail.com>
2021-12-01 20:36:59 -0800

    Tweak code.
    - Went with Boxed Error to make use of the ? operator
    - switched over to windows function, looks more functional than imperative

--- rust-2021/src/day_1/bin/main.rs
+++ rust-2021/src/day_1/bin/main.rs
@@ -1,15 +1,15 @@
-
+use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-fn main() -> Result<(), String> {
-    let input = File::open("./rust-2021/inputs/day_1_1_input").expect("Can't load file");
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = File::open("./rust-2021/inputs/day_1_1_input")?;
     let mut buf_reader = BufReader::new(input);
+    let mut numbers = vec![];
 
-    let numbers: Vec<i32> = buf_reader.lines()
-        .map(|line| line.expect("Couldn't convert to string"))
-        .map(|line| line.parse().expect("couldn't convert to i32"))
-        .collect();
+    for line in buf_reader.lines() {
+        numbers.push(line?.parse::<i32>()?);
+    }
 
     part_1(&numbers);
     part_2(&numbers);
@@ -18,37 +18,23 @@ fn main() -> Result<(), String> {
 }
 
 fn part_1(input: &Vec<i32>) {
-    let mut counter = 0;
-    let mut prev = input[0];
-    for &n in input {
-        if n > prev {
-            counter += 1;
-        }
-
-        prev = n;
-    }
+    let counter = input.windows(2)
+        .fold(0, |acc, window| if window[0] < window[1] {
+            acc + 1
+        } else { acc });
 
     println!("{:?}", counter);
 }
 
 fn part_2(input: &Vec<i32>) {
-    let mut counter = 0;
-    let mut cur_sum = 0;
+    let sums = input.windows(3)
+        .map(|window| window.iter().sum::<i32>())
+        .collect::<Vec<i32>>();
 
-    for i in 0..input.len() {
-        if i < 3 {
-            cur_sum += input[i];
-            continue;
-        }
-
-        let new_sum = cur_sum - input[i - 3] + input[i];
-
-        if new_sum > cur_sum {
-            counter += 1;
-        }
-
-        cur_sum = new_sum;
-    }
+    let counter = sums.windows(2)
+        .fold(0, |acc, window| if window[0] < window[1] {
+            acc + 1
+        } else { acc });
 
     println!("{:?}", counter);
 }

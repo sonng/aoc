da92071e15af7c7fa7b8da7d12eb041001baf1f4
Son Nguyen <anh.s.nguyen@gmail.com>
2021-12-09 21:36:47 -0800

    Day 10, Part 2

--- rust-2021/src/day_10/bin/main.rs
+++ rust-2021/src/day_10/bin/main.rs
@@ -1,5 +1,4 @@
 use std::error::Error;
-use std::str::Chars;
 use aoc_utils::{Puzzle, run_all};
 use std::collections::HashMap;
 
@@ -8,9 +7,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 type Input = Vec<Vec<u8>>;
-type Output = i32;
+type Output = i64;
 
 struct Day10;
+
+
 
 impl Puzzle<Input, Output> for Day10 {
     fn parse(&self, contents: Vec<String>) -> Result<Input, Box<dyn Error>> {
@@ -20,7 +21,7 @@ impl Puzzle<Input, Output> for Day10 {
     }
 
     fn calculate_part_1(&self, input: &Input) -> Output {
-        let score_mapping  = HashMap::<u8, (u8, i32)>::from([
+        let score_mapping  = HashMap::<u8, (u8, i64)>::from([
             (')' as u8, ('(' as u8, 3)),
             (']' as u8, ('[' as u8, 57)),
             ('}' as u8, ('{' as u8, 1197)),
@@ -52,7 +53,49 @@ impl Puzzle<Input, Output> for Day10 {
     }
 
     fn calculate_part_2(&self, input: &Input) -> Output {
-        0
+        let score_mapping = HashMap::<u8, (u8, i64)>::from([
+            (')' as u8, ('(' as u8, 1)),
+            (']' as u8, ('[' as u8, 2)),
+            ('}' as u8, ('{' as u8, 3)),
+            ('>' as u8, ('<' as u8, 4))
+        ]);
+        let mapping = HashMap::<u8, u8>::from([
+            ('(' as u8, ')' as u8),
+            ('[' as u8, ']' as u8),
+            ('{' as u8, '}' as u8),
+            ('<' as u8, '>' as u8),
+        ]);
+
+        let mut scores = vec![];
+        'outer: for line in input {
+            let mut stack = vec![];
+            for c in line {
+                if score_mapping.contains_key(c) {
+                    let (open, _) = score_mapping[c];
+                    if let Some(last) = stack.last() {
+                        if open.eq(last) {
+                            stack.pop();
+                        } else {
+                            continue 'outer;
+                        }
+                    }
+                } else {
+                    stack.push(c.clone());
+                }
+            }
+
+            let mut calc_score = 0;
+            while let Some(item) = stack.pop() {
+                let op = mapping[&item];
+                let (_, score) = score_mapping[&op];
+                calc_score = (calc_score * 5) + score;
+            }
+
+            scores.push(calc_score);
+        }
+
+        scores.sort();
+        scores[scores.len() / 2]
     }
 }
 
@@ -69,7 +112,7 @@ mod test {
 
     #[test]
     fn test_part_two() -> Result<(), Box<dyn Error>> {
-        assert_eq!(0, run_part_two("./inputs/day_10_test.in", Box::new(Day10 {}))?);
+        assert_eq!(288957, run_part_two("./inputs/day_10_test.in", Box::new(Day10 {}))?);
         Ok(())
     }
 }

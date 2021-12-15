71939da8cce3858f93745e03345730dda50416e1
Son Nguyen <anh.s.nguyen@gmail.com>
2021-12-14 23:10:40 -0800

    Day 15 - Part 2
    Initial DP approach didn't work for the bigger map since it could only
    go to the right or down.
    
    Went for shortest path by cost approach with min-heap.

--- rust-2021/src/day_15/bin/main.rs
+++ rust-2021/src/day_15/bin/main.rs
@@ -1,4 +1,5 @@
-use std::cmp::min;
+use std::cmp::{min, Ordering};
+use std::collections::BinaryHeap;
 use std::error::Error;
 use aoc_utils::{Puzzle, run_all};
 
@@ -11,14 +12,27 @@ struct Day15;
 type Input = Vec<Vec<u32>>;
 type Output = u32;
 
-impl Puzzle<Input, Output> for Day15 {
-    fn parse(&self, contents: Vec<String>) -> Result<Input, Box<dyn Error>> {
-        Ok(contents.iter()
-            .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>())
-            .collect::<Vec<Vec<_>>>())
-    }
+#[derive(Eq, PartialEq, Clone, Copy)]
+struct State {
+    cost: u32,
+    position: (i32, i32)
+}
 
-    fn calculate_part_1(&self, input: &Input) -> Output {
+impl PartialOrd<Self> for State {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.cost.cmp(&self.cost)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl Day15 {
+    fn find_shortest(&self, input: &Input) -> Output {
         let height = input.len();
         let width = input[0].len();
         let mut dp = vec![vec![0 as u32; width]; height];
@@ -47,8 +61,78 @@ impl Puzzle<Input, Output> for Day15 {
         dp[height - 1][width - 1]
     }
 
+    fn find_shortest_any_movement(&self, input: &Input) -> Output {
+        let height = input.len() as i32;
+        let width = input[0].len() as i32;
+        let end = (width - 1, height - 1);
+        let mut seen_costs = vec![vec![u32::MAX; width as usize]; height as usize];
+
+        let mut heap = BinaryHeap::new();
+        heap.push(State { cost: 0, position: (0, 0) });
+
+        while let Some(State { cost, position: (x, y) }) = heap.pop() {
+            if (x, y) == end { return cost; }
+            if cost > seen_costs[y as usize][x as usize] { continue; }
+
+            let moves = vec![(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)];
+            for (move_x, move_y) in moves {
+                if move_x < 0 || move_y < 0 || move_x >= width || move_y >= height { continue; }
+
+                let next = State { cost: cost + input[move_y as usize][move_x as usize],
+                                   position: (move_x, move_y) };
+
+                if next.cost < seen_costs[move_y as usize][move_x as usize] {
+                    heap.push(next);
+                    seen_costs[move_y as usize][move_x as usize] = next.cost;
+                }
+            }
+        }
+
+        panic!("For some reason we can't find it");
+    }
+}
+
+impl Puzzle<Input, Output> for Day15 {
+    fn parse(&self, contents: Vec<String>) -> Result<Input, Box<dyn Error>> {
+        Ok(contents.iter()
+            .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>())
+            .collect::<Vec<Vec<_>>>())
+    }
+
+    fn calculate_part_1(&self, input: &Input) -> Output {
+        self.find_shortest(input)
+    }
+
     fn calculate_part_2(&self, input: &Input) -> Output {
-        0
+        let height = input.len();
+        let width = input[0].len();
+
+        let new_height = height * 5;
+        let new_width = width * 5;
+
+        let mut new_input = vec![vec![0 as u32; new_width]; new_height];
+
+        for y in 0..new_height {
+            for x in 0..new_width {
+                let ref_y = y % height;
+                let ref_x = x % width;
+
+                let win_y = y / height;
+                let win_x = x / width;
+
+                if win_x == 0 && win_y == 0 {
+                    new_input[y][x] = input[y][x];
+                } else if win_x == 0 {
+                    let value = (new_input[ref_y][x] + (1 * win_y as u32) - 1) % 9;
+                    new_input[y][x] = value + 1;
+                } else {
+                    let value = (new_input[y][ref_x] + (1 * win_x as u32) - 1) % 9;
+                    new_input[y][x] = value + 1;
+                }
+            }
+        }
+
+        self.find_shortest_any_movement(&new_input)
     }
 }
 
@@ -65,7 +149,7 @@ mod test {
 
     #[test]
     fn test_calculate_two() -> Result<(), Box<dyn Error>> {
-        assert_eq!(0, run_part_two("./inputs/day_15_test.in", Box::new(Day15 {}))?);
+        assert_eq!(315, run_part_two("./inputs/day_15_test.in", Box::new(Day15 {}))?);
         Ok(())
     }
 }

4410da8bcfab5441f2b7825598122c11156ae009
Son Nguyen <anh.s.nguyen@gmail.com>
2021-12-14 21:22:31 -0800

    Day 15 - Part 1

--- rust-2021/src/day_15/bin/main.rs
+++ rust-2021/src/day_15/bin/main.rs
@@ -1,3 +1,4 @@
+use std::cmp::min;
 use std::error::Error;
 use aoc_utils::{Puzzle, run_all};
 
@@ -7,16 +8,43 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 struct Day15;
 
-type Input = Vec<i32>;
-type Output = i32;
+type Input = Vec<Vec<u32>>;
+type Output = u32;
 
 impl Puzzle<Input, Output> for Day15 {
     fn parse(&self, contents: Vec<String>) -> Result<Input, Box<dyn Error>> {
-        Ok(vec![])
+        Ok(contents.iter()
+            .map(|s| s.chars().map(|c| c.to_digit(10).unwrap()).collect::<Vec<_>>())
+            .collect::<Vec<Vec<_>>>())
     }
 
     fn calculate_part_1(&self, input: &Input) -> Output {
-        0
+        let height = input.len();
+        let width = input[0].len();
+        let mut dp = vec![vec![0 as u32; width]; height];
+
+        let mut y = 0;
+        while y < height as i32 {
+            let yu = y as usize;
+            let mut x = if y == 0 { 1 } else { 0 };
+            while x < width as i32 {
+                let xu = x as usize;
+
+                if y == 0 {
+                    dp[yu][xu] = dp[yu][xu - 1] + input[yu][xu];
+                } else if x == 0 {
+                    dp[yu][xu] = dp[yu - 1][xu] + input[yu][xu];
+                } else {
+                    dp[yu][xu] = min(dp[yu - 1][xu], dp[yu][xu - 1]) + input[yu][xu];
+                }
+
+                x += 1;
+            }
+
+            y += 1;
+        }
+
+        dp[height - 1][width - 1]
     }
 
     fn calculate_part_2(&self, input: &Input) -> Output {
@@ -31,7 +59,7 @@ mod test {
 
     #[test]
     fn test_calculate_one() -> Result<(), Box<dyn Error>> {
-        assert_eq!(0, run_part_one("./inputs/day_15_test.in", Box::new(Day15 {}))?);
+        assert_eq!(40, run_part_one("./inputs/day_15_test.in", Box::new(Day15 {}))?);
         Ok(())
     }
 
